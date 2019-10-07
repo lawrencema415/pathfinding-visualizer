29c9ac11964e175ba661d458c751fe9c8eb63fdb
Larry <lawrencema415@gmail.com>
2019-10-06 19:44:24 -0700

    adding grids

--- src/PathfindingVisualizer/PathfindingVisualizer.jsx
+++ src/PathfindingVisualizer/PathfindingVisualizer.jsx
@@ -6,15 +6,33 @@ import './PathfindingVisualizer.css';
 export default class PathfindingVisualizer extends Component {
   constructor(props) {
     super(props);
-    this.state={};
+    this.state={
+      nodes:[],
+    };
     }
 
+    componentDidMount() {
+      const nodes = [];
+      for(let row = 0; row < 15; row++) {
+        const currentRow = [];
+        for(let col = 0;col < 50;col++) {
+          currentRow.push([]);
+        }
+        nodes.push(currentRow)
+      }
+      this.setState({nodes});
+    }
 
     render() {
+      const {nodes} = this.state;
+
       return (
-        <div>
-        Setup
-          <Node/>
+        <div className="grid">
+          {nodes.map((row,rowIdx) => {
+            return <div key={rowIdx}>
+            {row.map((node,nodeIdx) => <Node/> )}
+            </div>
+          })}
         </div>
       )
     }
